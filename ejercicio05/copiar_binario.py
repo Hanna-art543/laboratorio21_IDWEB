44562009a50f265e389c3d759a215682a5377e73
Ana Miranda <amirandalo@unsa.edu.pe>
2025-12-15 23:06:26 -0500

    Subir ejercicio 05

---
+++ ejercicio05/copiar_binario.py
@@ -0,0 +1,21 @@
+def copiar_archivo_binario(origen, destino):
+    try:
+        with open (origen, "rb") as archivo_origen:
+            contenido = archivo_origen.read()
+        
+        with open (destino, "wb") as archivo_destino:
+            archivo_destino.write(contenido)
+        
+        print("Archivo binario copiado correctamente.")
+    
+    except FileNotFoundError:
+        print("Error: el archivo de origen no existe.")
+    
+    except IOError:
+        print("Error al copiar el archivo binario")
+    
+
+origen = input("Nombre del archivo binario origen: ")
+destino = input("Nombre del archivo binario destino: ")
+
+copiar_archivo_binario(origen, destino)
---
+++ ejercicio05/copiar_texto.py
@@ -0,0 +1,20 @@
+def copiar_archivo_txto(origen, destino):
+    try:
+        with open (origen, "r", encoding ="utf-8") as archivo_origen:
+            contenido = archivo_origen.read()
+        
+        with open (destino, "w", encoding = "utf-8") as archivo_destino:
+            archivo_destino.write(contenido)
+        
+        print("Archivo de texto copiado correctamente.")
+
+    except FileNotFoundError:
+        print("Error: el archivo de origen no existe")
+    
+    except IOError:
+        print("Error a leer o escribir el archivo")
+
+origen = input("Nombre del archivo de texto origen: ")
+destino = input("Nombre del archivo de texto destino: ")
+
+copiar_archivo_txto(origen, destino)
