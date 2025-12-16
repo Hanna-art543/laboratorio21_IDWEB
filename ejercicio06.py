91075f4ad949f0100afe246780309d38eedf3f52
Ana Miranda <amirandalo@unsa.edu.pe>
2025-12-15 23:12:30 -0500

    Subir ejercicio 06

---
+++ ejercicio06.py
@@ -0,0 +1,27 @@
+import json
+
+equipos = [
+    {
+        "Nombre": "Real Madrid",
+        "país": "España",
+        "nivelAtaque": 90,
+        "nivelDefensa": 85
+    },
+    {
+        "Nombre": "Manchester City",
+        "país": "Inglaterra",
+        "nivelAtaque": 92,
+        "nivelDefensa": 88
+    },
+    {
+        "Nombre": "Bayern Múnich",
+        "país": "Alemania",
+        "nivelAtaque": 89,
+        "nivelDefensa": 84
+    }
+]
+
+
+json_equipos = json.dumps(equipos, indent=4, ensure_ascii=False)
+
+print(json_equipos)
